c8f6b524fd2acb7baa3fc8e1ec5a1c16d629dc8c
kamiloC <kamilo.sniper@gmail.com>
2020-02-29 01:36:09 -0500

    Initialize the repository

---
+++ components/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1 class="head__title">Nabucodonosor</h1>
+    <h2 class="head__page">{{ pageTitle }}</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    pageTitle: {
+      type: String,
+      required: true,
+      default: 'Inicio'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.head {
+  &__title,
+  &__page {
+    margin: 1% auto;
+    text-align: center;
+    width: 100%;
+  }
+
+  &__title {
+    color: $primaryText;
+    font-size: 2em;
+  }
+
+  &__page {
+    font-size: 1.2em;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <div>
+      <div class="content-logos">
+        <logo />
+      </div>
+      <h1 class="title">
+        Nuxt.js
+      </h1>
+      <h2 class="subtitle">
+        <a href="https://nuxtjs.org/">Nuxt</a> s a progressive framework based
+        on <a href="https://vuejs.org/">Vuejs</a> to create modern web
+        applications. It is based on Vue.js official libraries (vue, vue-router
+        and vuex) and powerful development tools (webpack, Babel and PostCSS).
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 5% auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  display: block;
+  font-weight: 300;
+  font-size: 55px;
+  color: #35495e;
+  letter-spacing: 1px;
+  text-transform: capitalize;
+  margin: 25px 0px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 1.1rem;
+  color: #526488;
+  word-spacing: 2px;
+  padding-bottom: 15px;
+  max-width: 600px;
+}
+
+.subtitle a {
+  font-weight: 500;
+  color: inherit;
+}
+
+.content-logos {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 500px;
+}
+
+.h3 {
+  font-weight: 400;
+  margin: 10px;
+}
+</style>
